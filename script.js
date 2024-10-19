10a4667fed4e8a2985b3ddef784269709d04688a
ctdth13 <ctdth99@gmail.com>
2024-10-19 16:56:43 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+console.log('Привет!');
+console.log(123)
+console.log('привет еще раз!', 'heey')
+
+//однострочный коммент
+
+/*многострочный
+коммент*/
+
+let message = 'how are you?'
+//сначала переменная объявляется, а потом используется и ее можно переопределить
+console.log(message)
+
+message = 'bye!'
+console.log(message)
+
+const letter = 'experiment'
+//const нельзя переопределить
+
+const
+    name = 'Nastya',
+    age = '24'
+
+const admin = name
+console.log(admin)
+
+/*имя переменной можно писать в любом регистре и с любыми символами,
+можно использовать подчеркивания цифры и знаки , 
+но имя переменной не может начинаться с цифры */
